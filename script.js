db47f57128f3a2ce48fbab8cb8d6c46da8777675
Malu Fernandes <85230561+malufernandes@users.noreply.github.com>
2021-07-22 15:53:27 -0300

    add.arquivos

---
+++ script.js
@@ -0,0 +1,24 @@
+function carregar(){
+
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} horas!`
+
+if(hora >= 0 && hora < 12){
+    //BOM DIA
+    img.src= 'dia.png'
+    document.body.style.background = '#89CCFA';
+}else if(hora >= 12 && hora < 18){
+    //BOA TARDE
+    img.src= 'tarde.png'
+    document.body.style.background = '8599B2';
+}else{
+    //BOA NOITE
+    img.src= 'noite.png'
+    document.body.style.background = '#0E1220';
+}
+
+}
